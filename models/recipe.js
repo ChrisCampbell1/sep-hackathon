e2379ffa6f22f060c95dc5458eb9def47ff38592
Max Walker <maxwalker23@gmail.com>
2023-09-15 19:07:10 -0400

    recipe model added

---
+++ models/recipe.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const recipeSchema = new Schema({
+  title: String,
+  image: String,
+  author: {type: Schema.Types.ObjectId, ref: 'Profile'},
+  description: String,
+  audio: String,
+  recipeYield: Number,
+  recipeCategory: String,
+    recipeCuisine: String,
+    ingredients: [String],
+    instructions: [String],
+},{
+  timestamps: true,
+})
+
+const Recipe = mongoose.model('Recipe', recipeSchema)
+
+export { Recipe }
