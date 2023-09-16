96d4bc50bb037c46f349646f319071358beebe00
Max Walker <maxwalker23@gmail.com>
2023-09-15 20:51:30 -0400

    edit and delete routes

--- routes/recipes.js
+++ routes/recipes.js
@@ -12,8 +12,8 @@ router.use(decodeUserFromToken)
 router.get('/', checkAuth, recipesCtrl.index)
 router.get('/:id', checkAuth, recipesCtrl.show)
 router.post('/', checkAuth, recipesCtrl.create)
+router.put('/:id', checkAuth, recipesCtrl.update)
+router.delete('/:id', checkAuth, recipesCtrl.deleteRecipe)
 // router.put('/:id/add-photo', checkAuth, recipesCtrl.addPhoto)
-// router.put('/:id', checkAuth, recipesCtrl.update)
-// router.delete('/:id', checkAuth, recipesCtrl.deleteRecipe)
 
 export { router }
