4a2e4aebab330d09250c4b90c95b1d175fb227e0
Chris Campbell <christopherecampbell@gmail.com>
2023-09-16 22:07:28 -0600

    added data massaging to add authoer based on user token for recipe creation

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -24,6 +24,7 @@ const show = async (req, res) => {
 
 const create = async (req, res) => {
   try {
+    req.body.author = req.user.profile
     const recipe = await Recipe.create(req.body)
     res.status(200).json(recipe)
   } catch (err) {
