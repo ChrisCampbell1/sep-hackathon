2b10c5ccedfe852f0d3208816d52a8cf9a675d55
Chris Campbell <christopherecampbell@gmail.com>
2023-09-17 20:05:58 -0600

    add approve family member

--- routes/profiles.js
+++ routes/profiles.js
@@ -12,5 +12,7 @@ router.use(decodeUserFromToken)
 router.get('/', checkAuth, profilesCtrl.index)
 router.get('/:id', checkAuth, profilesCtrl.show)
 router.put('/:id/add-photo', checkAuth, profilesCtrl.addPhoto)
+router.put('/:id/request', checkAuth, profilesCtrl.request)
+router.put('/:id/approve', checkAuth, profilesCtrl.approve)
 
 export { router }
